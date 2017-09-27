f7a57d6a7c50b5217dfd6be3d2a6dd8726144271
Charles Fisher <cfisher440@gmail.com>
2017-09-27 16:38:09 +0000

    lecture 92

--- client/components/app.js
+++ client/components/app.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
 import Header from './header';
-import BinsList from './bins/bins_list';
 
 export default () => {
     return (
         <div>
             <Header />
-            <BinsList />
         </div>
         );
 };
